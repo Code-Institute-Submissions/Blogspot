06099ea7ab70e36d8f496da148fe958213c3c016
JTeo99 <jteo99@hotmail.com>
2024-03-26 22:47:14 +0000

    comment model added

--- core/models.py
+++ core/models.py
@@ -44,3 +44,63 @@ class Post(models.Model):
         return reverse('post_detail', args=[str(self.slug)])
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'Post', on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    name = models.CharField(max_length=80, blank=True)
+    email = models.EmailField(blank=True)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
+    dislikes = models.ManyToManyField(User, related_name='comment_dislikes', blank=True)
+    edited_on = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.user.username}"
+
+    def like(self, user):
+        self.likes.add(user)
+
+    def unlike(self, user):
+        self.likes.remove(user)
+
+    def dislike(self, user):
+        self.dislikes.add(user)
+
+    def undislike(self, user):
+        self.dislikes.remove(user)
+
+    @property
+    def likes_count(self):
+        return self.likes.count()
+
+    @property
+    def dislikes_count(self):
+        return self.dislikes.count()
+    
+    def edit(self, body):
+        self.body = body
+        self.edited_on = timezone.now()
+        self.save()
+
+    def delete(self):
+        self.is_deleted = True
+        self.save()
+
+    def is_reply(self):
+        return self.parent is not None
+
+    def get_reply_depth(self):
+        depth = 0
+        parent_comment = self.parent
+        while parent_comment is not None:
+            depth += 1
+            parent_comment = parent_comment.parent
+        return depth
