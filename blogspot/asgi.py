d26b78e3069761a67899d7f5bd3234f3184942ba
JTeo99 <jteo99@hotmail.com>
2024-03-27 10:49:31 +0000

    typos corrected

--- blogspot/asgi.py
+++ blogspot/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for blogpsot project.
+ASGI config for blogspot project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- blogspot/wsgi.py
+++ blogspot/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for blogpsot project.
+WSGI config for blogspot project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
