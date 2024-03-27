0bf47148c661e1b95795c228d988e9aa9318a9d3
JTeo99 <jteo99@hotmail.com>
2024-03-27 10:24:09 +0000

    blogspot typo within code preventing launching site

--- blogspot/asgi.py
+++ blogspot/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogpsot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogspot.settings')
 
 application = get_asgi_application()
