ab7d01e472855a3e914620d7367cdfa24d7583d7
JTeo99 <jteo99@hotmail.com>
2024-03-27 21:58:16 +0000

    remove comment urls

--- core/urls.py
+++ core/urls.py
@@ -11,9 +11,6 @@ urlpatterns = [
     # Post URLs
     path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
     
-    # Comment URLs
-    path('<slug:slug>/comment/', views.CommentCreateView.as_view(), name='comment_create'),
-    path('<slug:slug>/comment/<int:comment_id>/reply/', views.CommentReplyView.as_view(), name='comment_reply'),
     
     
 ]
