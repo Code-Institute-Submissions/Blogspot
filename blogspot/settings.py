1a41f9150e70511f8ac0dd05a1817645a1de6164
JTeo99 <jteo99@hotmail.com>
2024-03-28 11:10:10 +0000

    moving error html pages to root template

--- blogspot/settings.py
+++ blogspot/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jteo99-blogspot-2qi3sywbow.us1.codeanyapp.com', 
+ALLOWED_HOSTS = ["localhost", "127.0.0.1",'8000-jteo99-blogspot-2qi3sywbow.us1.codeanyapp.com', 
                 '.herokuapp.com'
 ]
 
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
