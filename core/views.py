7597ad9035deba4777aee667f77aa4590e071a49
JTeo99 <jteo99@hotmail.com>
2024-03-28 09:37:51 +0000

    syntax errors fixed

--- core/views.py
+++ core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
-from .forms import CommentForm, PostForm SignupForm LoginForm
+from .forms import CommentForm, PostForm, SignupForm, LoginForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator
-from django.db.models import Q
+from django.db.models impor t Q
 from django.contrib.auth import authenticate, login
 
 # Create your views here.
