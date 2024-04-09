afa44988a7d194536e10f6dc158ba447b3968a96
JTeo99 <jteo99@hotmail.com>
2024-04-09 18:47:39 +0000

    collectstatic

---
+++ staticfiles/js/script.js
@@ -0,0 +1,8 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#searchBtn').click(function () {
+            $('#searchInput').toggleClass('d-none d-md-block').focus();
+        });
+    });
+</script>
